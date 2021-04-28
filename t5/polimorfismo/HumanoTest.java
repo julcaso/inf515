ee5ced70c41ff9c057e520e0bda336fe44cbf634
julcaso <julioenriquecastro@gmail.com>
2021-04-28 13:53:35 -0400

    Add files via upload

---
+++ t5/polimorfismo/HumanoTest.java
@@ -0,0 +1,9 @@
+public class HumanoTest{
+    public static void main(String [] args){
+        Humano hm1 = new Hombre();
+        Humano hm2 = new Mujer();
+
+        hm1.hablar();
+        hm2.hablar();
+    }
+}
