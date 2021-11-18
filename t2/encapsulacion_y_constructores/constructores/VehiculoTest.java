cb0635e53c1adfc51fbfe2af81506b765982e9ac
julcaso <julioenriquecastro@gmail.com>
2021-11-18 09:11:25 -0400

    Update VehiculoTest.java

--- t2/encapsulacion_y_constructores/constructores/VehiculoTest.java
+++ t2/encapsulacion_y_constructores/constructores/VehiculoTest.java
@@ -1,11 +1,16 @@
 public class VehiculoTest{
-    public static void main (String [] arg){
-        Vehiculo miVehiculo = new Vehiculo();
-        miVehiculo.setColor("Gris");
-        miVehiculo.setVelocidadMaxima(260);
-        miVehiculo.desplegarPantalla();
+    public static void main (String [] args){
+        Vehiculo miVehiculo1 = new Vehiculo();
+        Vehiculo miVehiculo2 = new Vehiculo("Negro", 200,"avion");
+        Vehiculo miVehiculo3 = new Vehiculo();
 
-        Vehiculo miVehiculo2 = new Vehiculo("Jeep SUV", 290, "Azul");
-        miVehiculo2.desplegarPantalla();
+        miVehiculo3.setColor("Negro");
+        miVehiculo3.setVelocidadMaxima(250);
+        miVehiculo3.setCantidadDePuertas(5);
+        miVehiculo3.setTipoVehiculo("Jeepeta");
+
+        miVehiculo1.desplegarInformacion();
+        miVehiculo2.desplegarInformacion();
+        miVehiculo3.desplegarInformacion();
     }
 }
