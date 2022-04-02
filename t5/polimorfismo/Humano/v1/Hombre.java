0fc256e91aae550c19f5069872becfd209752de4
julcaso <julioenriquecastro@gmail.com>
2022-04-02 09:11:43 -0400

    Add files via upload

---
+++ t5/polimorfismo/Humano/v1/Hombre.java
@@ -0,0 +1,6 @@
+public class Hombre extends Humano{
+    public void hablar() {
+        super.hablar();
+        System.out.println("Hombre hablando.");
+    }
+}
---
+++ t5/polimorfismo/Humano/v2/Mujer.java
@@ -0,0 +1,12 @@
+public class Mujer extends Humano{
+    public void hablar(){
+        System.out.println("Mujer hablando.");
+        System.out.println("Mujer trabajando.");
+        System.out.println("Mujer cocinando.");
+        System.out.println("N operaciones mas.\n");
+    }
+
+    public void caminar(){
+        System.out.println("Mujer caminando.\n");
+    }
+}
