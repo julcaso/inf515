09ce3c0c6d07940981f5669b864ba8312fb945a0
Julio Castro Ortiz <julioenriquecastro@gmail.com>
2023-09-24 14:37:31 -0400

    Update EntradaDeDatos.java

--- t1/iniciando_con_java/EntradaDeDatos.java
+++ t1/iniciando_con_java/EntradaDeDatos.java
@@ -1,3 +1,19 @@
+/***************************************
+Autor:    Julio Castro Ortiz.
+Fecha:    24/9/2023.
+Semestre: 2023-20.
+Objetivo: Ejemplo docente.
+
+Este programa es simple pero ilustra los conceptos básicos de la 
+entrada de datos con Scanner. En primer lugar, crea un objeto Scanner 
+para leer datos del teclado. Luego, utiliza el método nextLine() para
+leer una  cadena de texto  del  usuario. Por último, utiliza el método 
+nextInt() para leer un número entero del usuario.
+
+Tema:     Iniciando con Java.
+Materias: INF514, INF515.
+****************************************/
+
 import java.util.Scanner;
 
 public class EntradaDeDatos {
